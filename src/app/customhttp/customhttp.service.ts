7283a9b7952d00841daa4120887cd1bf9761d51e
lekkanm <lekkanm@gmail.com>
2018-05-21 18:40:10 +0530

    Updated version changes

--- src/app/customhttp/customhttp.service.ts
+++ src/app/customhttp/customhttp.service.ts
@@ -1,9 +1,9 @@
-import { Injectable } from '@angular/core'
-import { Http,Response} from '@angular/http';
+import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/do';
 import {IProduct} from './product';
+import {Http,Response} from '@angular/http';
 
 @Injectable()
 export class CustomhttpService{
--- src/app/register/register.service.ts
+++ src/app/register/register.service.ts
@@ -1,9 +1,8 @@
-import { Injectable } from '@angular/core'
-import { Http,Response} from '@angular/http';
+import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/do';
-
+import { Http,Response} from '@angular/http';
 
 @Injectable()
 export class RegisterService{
